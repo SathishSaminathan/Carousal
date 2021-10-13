49c1a0e8acf1f400096b87ff8e431ca4b79fdce3
SathishSaminathan <SathishSaminathan@kredx.com>
2021-10-13 15:46:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Carousel from './Carousel';
+import './style.css';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
+export default function App() {
+  return <Carousel />;
+}
---
+++ src/Carousel.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+
+export default () => (
+  <Carousel autoPlay showThumbs={false} showIndicators={false}>
+    <div>
+      <img alt="" src={"./backbone.jpeg"} />
+      {/* <p className="legend">Legend 1</p> */}
+    </div>
+    <div>
+      <img alt="" src="./canvas.jpeg" />
+      {/* <p className="legend">Legend 2</p> */}
+    </div>
+    <div>
+      <img alt="" src="./css.png" />
+      {/* <p className="legend">Legend 3</p> */}
+    </div>
+    <div>
+      <img alt="" src="./emeber.png" />
+      {/* <p className="legend">Legend 4</p> */}
+    </div>
+    <div>
+      <img alt="" src="./flutter.jpeg" />
+      {/* <p className="legend">Legend 5</p> */}
+    </div>
+    <div>
+      <img alt="" src="./html.jpg" />
+      {/* <p className="legend">Legend 6</p> */}
+    </div>
+    <div>
+      <img alt="" src="./jquery.png" />
+      {/* <p className="legend">Legend 7</p> */}
+    </div>
+    <div>
+      <img alt="" src="./konva.png" />
+      {/* <p className="legend">Legend 8</p> */}
+    </div>
+    <div>
+      <img alt="" src="./React_Native.jpg" />
+      {/* <p className="legend">Legend 9</p> */}
+    </div>
+    <div>
+      <img alt="" src="./reactjs-thumb.jpg" />
+      {/* <p className="legend">Legend 10</p> */}
+    </div>
+    <div>
+      <img alt="" src="./sass.png" />
+      {/* <p className="legend">Legend 11</p> */}
+    </div>
+    <div>
+      <img alt="" src="./svelte-logo.png" />
+      {/* <p className="legend">Legend 12</p> */}
+    </div>
+    <div>
+      <img alt="" src="./the-seo-guide-to-angular-760x400.png" />
+      {/* <p className="legend">Legend 13</p> */}
+    </div>
+    <div>
+      <img alt="" src="./trending-css-frameworks.png" />
+      {/* <p className="legend">Legend 14</p> */}
+    </div>
+    <div>
+      <img alt="" src="./vue.png" />
+      {/* <p className="legend">Legend 14</p> */}
+    </div>
+  </Carousel>
+);
